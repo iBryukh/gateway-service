236e355128be1750f814ad2c75c1d58f1a7d96cc
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-06 21:30:06 +0200

    Remove redundant test object from bootable class
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/smarthouse/gateway/GatewayApplication.java
+++ src/main/java/com/smarthouse/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
 package com.smarthouse.gateway;
 
-import com.smarthouse.commonutil.TestObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		TestObject testObject = new TestObject(null);
-		System.out.println(testObject.getName());
-		System.out.println(TestObject.getHelloString());
-
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
