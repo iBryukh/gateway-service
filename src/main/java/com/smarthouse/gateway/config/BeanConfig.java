5e2e534ff13c0f3342dc0f771ba1fae85b1d9231
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-14 20:50:31 +0200

    Implement pre filtering
    - Create bean config which will provide:
     - UserContext
     - UserContextFilter
     - RestTemplate which will use ribbon and will contains userContextInterceptor
    - Add util package as in example of spmia 6 chapter
    
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/smarthouse/gateway/config/BeanConfig.java
@@ -0,0 +1,43 @@
+package com.smarthouse.gateway.config;
+
+import com.smarthouse.gateway.utils.UserContext;
+import com.smarthouse.gateway.utils.UserContextFilter;
+import com.smarthouse.gateway.utils.UserContextInterceptor;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public UserContext getUserContext() {
+        return new UserContext();
+    }
+
+    @Bean
+    public UserContextFilter getUserContextFilter() {
+        return new UserContextFilter();
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors == null) {
+            template.setInterceptors(
+                    Collections.<ClientHttpRequestInterceptor>singletonList(new UserContextInterceptor())
+            );
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
+}
